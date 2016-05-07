d497b216e4b76f5a53eeafb6ae70ac94751a8617
lzh <lizhaohui1991@gmail.com>
2016-05-07 13:49:44 +0800

    evil-matchit and evil-nerd-commenter

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -79,9 +79,7 @@ buffer is not visiting a file."
 ;;                 (rename-uniquely)))))))
 
 (evil-leader/set-key
-  "sc" 'async-shell-command
-  "cr" 'comment-region
-  "ur" 'uncomment-region)
+  "sc" 'async-shell-command)
 
 ;; server
 (use-package server
@@ -102,5 +100,23 @@ buffer is not visiting a file."
 ;;; TODO
 (require 'init-dired) ;; only affect dired-mode
 (require 'init-ibuffer) ;; only affect ibuffer-mode
+
+(use-package evil-nerd-commenter
+  :config
+  (global-set-key (kbd "M-;") 'evilnc-comment-or-uncomment-lines)
+  (evil-leader/set-key
+    "ci" 'evilnc-comment-or-uncomment-lines
+    "cl" 'evilnc-quick-comment-or-uncomment-to-the-line
+    "ll" 'evilnc-quick-comment-or-uncomment-to-the-line
+    "cc" 'evilnc-copy-and-comment-lines
+    "cp" 'evilnc-comment-or-uncomment-paragraphs
+    "cr" 'comment-or-uncomment-region
+    "cv" 'evilnc-toggle-invert-comment-line-by-line
+    "\\" 'evilnc-comment-operator       ; if you prefer backslash key
+    ))
+
+(use-package evil-matchit
+  :config
+  (global-evil-matchit-mode 1))
 
 (provide 'init-misc)
