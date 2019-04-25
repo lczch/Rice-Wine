0cfb72c3d94137c63384f1607c7542480a461429
lzh <lizhaohui1991@gmail.com>
2019-04-25 21:11:31 +0800

    修改了ivy TAB的行为

--- lib/rw-misc-lib.el
+++ lib/rw-misc-lib.el
@@ -2,8 +2,11 @@
 ;; misc functions
 ;;------------------------------------------------------------------------------
 (defun rw-test-new-config ()
-  "Async open a new emacs, with current file opened"
+  "Async open a new emacs, with current file opened."
   (interactive)
+  ;; (when (f-exists? rice-wine-configure-file)
+  ;;   (org-babel-tangle-file rice-wine-configure-file))
+  ;; start emacs
   (let ((file (buffer-file-name)))
     (async-shell-command (concat "emacs " file " --debug"))))
 
