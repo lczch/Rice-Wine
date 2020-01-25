debfa69b9dc8a69605c86ab93d63a0a872bd0a13
lczch <lizhaohui1991@gmail.com>
2020-01-25 21:05:11 +0800

    clean a little

---
+++ init.el
@@ -0,0 +1,91 @@
+;; -*- coding: utf-8 -*-
+(defconst emacs-start-time (current-time))
+
+;;------------------------------------------------------------------------------
+;; Define global variables and set global load-path 
+;;------------------------------------------------------------------------------
+(defvar rice-wine-dir (file-name-directory load-file-name)
+  "top directory of configuration")
+
+;; 修改emacs默认的directory到自定义的目录
+(setq user-emacs-directory rice-wine-dir)
+
+(defvar rice-wine-lisp-dir (expand-file-name "lisp" rice-wine-dir)
+  "configurations of packages")
+
+(defvar rice-wine-package-dir
+  (expand-file-name "site-lisp" rice-wine-dir)
+  "local packages")
+
+(defvar rice-wine-git-package-dir
+  (expand-file-name "git-lisp" rice-wine-dir)
+  "packages from git, which have higher priority than pakages in `rice-wine-package-dir'")
+
+(defvar rice-wine-package-temp-dir
+  (expand-file-name "temp" rice-wine-dir)
+  "try packages from elpa and melpa")
+
+(defvar rice-wine-lib-dir
+  (expand-file-name "lib" rice-wine-dir)
+  "library packages, mostly for emacs-lisp programming")
+
+;; (defvar rice-wine-configure-file 
+;;   (expand-file-name "README.org" rice-wine-dir)
+;;   "Store the path of \"README.org\"")
+
+(defvar sys/win32p (eq system-type 'windows-nt))
+
+;; set load-path, include all packages and init files
+(defun rw-add-to-load-path (dir)
+  "add DIR to the head of load-path"
+  (interactive "DDir:")
+  (add-to-list 'load-path dir))
+
+(defun rw-add-subdirs-to-load-path (dir)
+  "add all subdirs of DIR to load-path, which begin with a digital, letter or '+'."
+  (let ((dir-files (directory-files dir t "^[0-9A-Za-z+].*")))
+    (dolist (file dir-files)
+      (when (file-directory-p file)
+        (rw-add-to-load-path file)))))
+
+(defun rw-add-dir-and-subdirs-to-load-path (dir)
+  "add DIR and all subdirs of DIR to load-path, which begin with a digital or letter."
+  (interactive "DDir:")
+  (rw-add-to-load-path dir)
+  (rw-add-subdirs-to-load-path dir))
+
+(let ((dirs (list
+             ;; init file for packages
+             rice-wine-lisp-dir
+             ;; packages from elpa
+             rice-wine-package-temp-dir
+             ;; local packages
+             rice-wine-package-dir
+             ;; I think useless
+             rice-wine-git-package-dir
+             ;; library writted by myself
+             rice-wine-lib-dir)))
+  (mapc #'rw-add-dir-and-subdirs-to-load-path dirs))
+
+(message "%s\n" load-path)
+;;------------------------------------------------------------------------------
+;; other init settings which should set before any functional packages 
+;;------------------------------------------------------------------------------
+(require 'pre-init)
+
+;;------------------------------------------------------------------------------
+;; various extensible functional packages which I prefer called "layer"
+;;------------------------------------------------------------------------------
+(require 'layers)
+
+;;------------------------------------------------------------------------------
+;; Post initialization: show the time finishing configuration
+;;------------------------------------------------------------------------------
+(when window-system
+  (add-hook 'after-init-hook
+            `(lambda ()
+               (let ((elapsed (float-time (time-subtract (current-time)
+                                                         emacs-start-time))))
+                 (message "Loading %s...done (%.3fs) [after-init]"
+                          ,load-file-name elapsed)))
+            t))
---
+++ lisp/layers.el
@@ -0,0 +1,191 @@
+;; 抄自以下文章，问好看起见，改了个名字。
+;; 参考资料： [[https://blog.csdn.net/fenxian2011/article/details/19254949]]
+(defun rw/prepend-to-exec-path (path)  
+  "push the path to the emacs internal `exec-path' and \"PATH\" env variable.  
+Return the updated `exec-path'"  
+  (setenv "PATH" (concat (expand-file-name path)  
+                         path-separator  
+                         (getenv "PATH")))  
+  (setq exec-path  
+        (cons (expand-file-name path)  
+              exec-path)))
+
+(use-package cygwin-mount
+  :config
+  (cygwin-mount-activate))
+
+(require 'init-elpa)
+(require 'init-straight)
+(require 'init-locales)
+;; configure the appearance of emacs
+(require 'init-modeline)
+(require 'init-theme)
+(require 'init-fonts)
+;; (require 'init-isearch)
+(require 'init-minibuff)
+(require 'init-windows)
+
+
+(require 'init-evil)
+(require 'init-dired)
+(require 'init-ibuffer)
+
+
+(require 'init-server)
+(require 'init-yasnippet)
+(require 'init-org-mode)
+(require 'init-company-mode)
+(require 'init-ivy-mode)
+
+(require 'init-evil-escape)
+(require 'init-programming)
+(require 'init-chinese-font)
+(require 'init-latex)
+(require 'init-eshell)
+(require 'init-english)
+
+;; Nicer naming of buffers for files with identical names
+(use-package uniquify
+  :config
+  (setq uniquify-buffer-name-style 'reverse)
+  (setq uniquify-separator " • ")
+  (setq uniquify-after-kill-buffer-p t)
+  (setq uniquify-ignore-buffers-re "^\\*"))
+
+(use-package init-info-mode
+  :mode (("\\.info\\'" . info-mode)))
+
+(use-package visual-regexp
+  :commands (vr/query-replace)
+  :init 
+  (evil-leader/set-key
+    "rr" 'vr/query-replace
+    ;; "vm" 'vr/mc-mark
+    ))
+
+;; expand-region: increase selected region by semantic units
+(use-package expand-region
+  :config
+  (evil-leader/set-key
+    "xx" 'er/expand-region)
+  
+  (setq expand-region-contract-fast-key "z")
+  (define-key evil-visual-state-map (kbd "v") 'er/expand-region)
+  )
+
+;; save place
+(use-package saveplace
+  :config
+  (setq-default save-place t))
+
+;; Highlight the cursor whenever the window scrolls
+;; beacon: need package "seq"
+(use-package beacon
+  :config
+  (beacon-mode 1))
+
+(use-package browse-kill-ring
+  :config
+  ;; no duplicates
+  (setq browse-kill-ring-display-duplicates nil)
+  ;; preview is annoying
+  (setq browse-kill-ring-show-preview nil)
+  (browse-kill-ring-default-keybindings)
+  (define-key evil-normal-state-map (kbd "M-y") 'browse-kill-ring)
+  ;; hotkeys:
+  ;; n/p => next/previous
+  ;; s/r => search
+  ;; l => filter with regex
+  ;; g => update/refresh
+  )
+
+;; TODO: may switch to gtags?
+(use-package init-xcscope)
+
+(use-package init-clipboard)
+
+(use-package which-key
+  :config
+  (which-key-mode 1))
+
+;; TODO: I use this seldom.
+(use-package init-emacs-w3m)
+
+;; TODO: I use this seldom.
+(use-package init-profiler)
+
+;;------------------------------------------------------------------------------
+;; about programming
+;;------------------------------------------------------------------------------
+(use-package init-markdown)
+
+(use-package init-haskell-mode)
+
+;; (use-package tex-mode
+;;   :init
+;;   (add-hook 'latex-mode-hook 'smartparens-mode)
+;;   (add-hook 'latex-mode-hook 'rainbow-delimiters-mode))
+
+;;------------------------------------------------------------------------------
+;; misc configurations
+;;------------------------------------------------------------------------------
+;; debug on
+(global-set-key (kbd "<f12>") 'toggle-debug-on-error)
+
+(evil-leader/set-key
+  "xh" 'mark-whole-buffer
+  "do" 'rw-display-current-buffer-other-frame
+  "eb" 'eval-buffer
+  "rb" 'revert-buffer) 
+
+(fset 'yes-or-no-p 'y-or-n-p)
+(setq history-delete-duplicates t)
+
+;; some basic preferences
+(setq-default buffers-menu-max-size 30
+              case-fold-search t
+              save-interprogram-paste-before-kill t
+              indent-tabs-mode nil
+              mouse-yank-at-point t
+              tooltip-delay 1.5
+              truncate-lines nil
+              truncate-partial-width-windows nil
+              ;; visible-bell has some issue
+              ;; @see https://github.com/redguardtoo/mastering-emacs-in-one-year-guide/issues/9#issuecomment-97848938
+              visible-bell nil)
+
+;; custom-file and backup-directory
+(setq auto-save-interval 50)
+(let ((my-custom-file (expand-file-name "custom.el" rice-wine-dir))
+      (my-backup-dir (expand-file-name "backups" rice-wine-dir)))
+  (setq custom-file my-custom-file)
+  (setq backup-directory-alist `(("." . ,my-backup-dir))))
+
+;; about Semantic
+(setq semanticdb-default-save-directory nil)
+
+;; (global-set-key (kbd "<f5>")
+;;                 #'(lambda ()
+;;                     (interactive)
+;;                     (semantic-grammar-create-package)
+;;                     (eval-buffer)))
+
+;; (global-set-key (kbd "<f6>")
+;;                 #'(lambda ()
+;;                     (interactive)
+;;                     (revert-buffer nil t)
+;;                     (bovinate)))
+
+;;------------------------------------------------------------------------------
+;; restore desktop
+;;------------------------------------------------------------------------------
+;; (when rw-main-emacs-p
+;;   (use-package init-desktop))
+
+;;------------------------------------------------------------------------------
+;; printer: we need to install "xpp" through os package manager
+;;------------------------------------------------------------------------------
+(setq lpr-command "xpp")
+
+
+(provide 'layers)
