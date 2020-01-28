843dc76f94fd563b9b4bfdbb9960602e0bd7f0b7
lczch <lizhaohui1991@gmail.com>
2020-01-28 20:39:38 +0800

    Try to use magit-evil
    还有些问题. 比如, 怎样让package不下载我本地已有的包(因为依赖)? 比如
    evil.

--- lisp/+git/init-magit.el
+++ lisp/+git/init-magit.el
@@ -1,16 +1,10 @@
 (use-package magit
   :ensure t
+  :defer t
   :init
   ;; 从melpa上安装的目录结构和git上的目录结构有差别.
   ;; 这个autoloads没有provide feature, 所以只能这样load
   (load-library "magit-autoloads")
-  ;; (load-file (expand-file-name "magit/magit-autoloads.el"
-  ;;                              rice-wine-package-temp-dir))
-
-  ;; (with-eval-after-load 'info
-  ;;   (info-initialize)
-  ;;   (add-to-list 'Info-directory-list
-  ;;                (expand-file-name "magit/Documentation" rice-wine-package-temp-dir)))
   :config
   ;; from redguardtoo
   (ivy-mode 1)
@@ -28,6 +22,19 @@
    "gm"  'magit-dispatch-popup
    "gs"  'magit-status
    "gS"  'magit-stage-file
-   "gU"  'magit-unstage-file))
+   "gU"  'magit-unstage-file)
+
+  (use-package evil-magit
+    ;; 这个包会导致安装evil, 而evil是我可能做了略微魔改的陈年stable版本, 不知道会不会出问题...
+    ;; 先试试吧. 如果出问题, 那就用straight安装这个包.
+    :ensure t
+    :init
+    ;; optional: this is the evil state that evil-magit will use
+    (setq evil-magit-state 'normal)
+    ;; optional: disable additional bindings for yanking text
+    ;; (setq evil-magit-use-y-for-yank nil)
+    :config
+    )
+  )
 
 (provide 'init-magit)
