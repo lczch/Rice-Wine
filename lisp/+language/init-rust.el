c92a29ec4e0865999278fe6dbee676c7589cf2dd
lczch <lizhaohui1991@gmail.com>
2020-01-29 10:35:15 +0800

    Add racer for rust-mode

--- lisp/+language/init-rust.el
+++ lisp/+language/init-rust.el
@@ -1,5 +1,7 @@
+;;; change-log
+;; 2020/1/29: install racer 
+
 ;;; TODO
-;; 还有rust completion的包racer, 但配置起来很复杂, 先暂时不用了.
 ;; 我觉得这两个包应该可以用melpa的, 不用从git hub上下. 
 
 (rw-straight-use-package 'rust-mode "rust-lang" "lczch")
@@ -9,12 +11,15 @@
 ;; (rw-straight-use-package 'flycheck-rust "flycheck" "lczch")
 
 (use-package rust-mode
-  :init 
+  :init
   (autoload 'rust-mode "rust-mode" nil t)
   (add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
   :defer t
   :config
   (use-package cargo)
+
+  (use-package racer
+    :ensure t)
 
   (defun rust-mode-func ()
     (smartparens-mode 1)
@@ -22,6 +27,8 @@
     (setup-company-mode '((company-capf company-tabnine)))
     (setq-local company-tooltip-align-annotations t)
     (cargo-minor-mode 1)
+    (racer-mode 1)
+    (eldoc-mode 1)
     )
   
   (add-hook 'rust-mode-hook 'rust-mode-func)
