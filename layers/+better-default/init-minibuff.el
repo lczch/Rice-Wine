9d947c781ce967cbbe197b50a2c0602b212bc1bd
lczch <rice-wine@foxmail.com>
2022-12-21 10:49:25 +0800

    don't change gc-cons-threshold easily

--- layers/+better-default/init-minibuff.el
+++ layers/+better-default/init-minibuff.el
@@ -4,13 +4,15 @@
   (smartparens-mode)
   (local-set-key (kbd "M-y") 'paste-from-x-clipboard)
   (local-set-key (kbd "C-k") 'kill-line)
-  (setq gc-cons-threshold most-positive-fixnum))
+  ;; (setq gc-cons-threshold most-positive-fixnum)
+  )
 
 (defun my-minibuffer-exit-hook ()
   ;; evil-mode also use minibuf
   ;; (conditionally-paredit-mode -1)
   (smartparens-mode 0)
-  (setq gc-cons-threshold best-gc-cons-threshold))
+  ;; (setq gc-cons-threshold best-gc-cons-threshold)
+  )
 
 ;; @see http://bling.github.io/blog/2016/01/18/why-are-you-changing-gc-cons-threshold/
 (add-hook 'minibuffer-setup-hook #'my-minibuffer-setup-hook)
